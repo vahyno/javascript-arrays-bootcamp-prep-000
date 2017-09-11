2ea8d4d32872b1ed4138e8aa5f89664aab3ac2f8
M V <vahyno+learn@gmail.com>
2017-09-11 22:21:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,9 +39,9 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0,array.lenght-1);
+  return array.slice(0,array.lenght-1);
   //return array.pop();
-  return array;
+  //return array;
 }
 
 console.log(removeElementFromEndOfArray(chocolateBars))
